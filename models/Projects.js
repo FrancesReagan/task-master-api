e54a22b8ecd26648e73fb7f27f9f66a8cfd3f746
Frances Reagan James <adtgwen@gmail.com>
2025-07-22 12:12:55 -0400

    added Project.js schema

---
+++ models/Projects.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from "mongoose";
+const projectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default:Date.now,
+  },
+  user: {
+    typs: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  tasks:[{
+    type: Schema.Types.ObjectId,
+    ref: "Task",
+  },],
+
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+export default Project;
