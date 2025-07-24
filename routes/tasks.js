6cd27a701f87ff3e7c5e6b7fa4bf16efeecaa41c
Frances Reagan James <adtgwen@gmail.com>
2025-07-23 22:33:25 -0400

    added get task by id route in tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -19,12 +19,34 @@ router.get("/", async (req, res) => {
     const projectIds = userProjects.map(project => project._id);
     // for those projects discover all associated tasks//
     const tasks = await Task.find({ project: { $in: projectIds }})
-    .populate("project", "name description")
+    .populate("project");
     .sort({ createdAt: -1});
 
     res.json(tasks);
 
   } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET  /api/tasks/:id  - retrieve a single task by its ID//
+router.get("/:id", async (req,res) => {
+  try {
+    const task = await Task.findById(req.params.id)
+    .populate("project");
+
+    if(!task) {
+      return res.status(404).json({ message: "Requested id has no task associated with it"})
+    }
+    if(!task.project) {
+      return res.status(404).json({ message: "Parent project not found"});
+    }
+    // auth check//
+    if (task.project.user.toString !== req.user._id.toString()) {
+      return res.status(403).json ({ message: "Authorization failed..You can not access this task." });
+    }  
+    res.json(task);
+  } catch  (error) {
     res.status(500).json(error);
   }
 });
