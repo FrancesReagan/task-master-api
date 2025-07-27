d4a5fdcaf78958741813745b86633b2424ad75a1
Frances Reagan James <adtgwen@gmail.com>
2025-07-27 12:57:26 -0400

    adding notes and praticing reading my code and writing out the logic and flow of what it means--proud I have reached this point of ability

--- routes/tasks.js
+++ routes/tasks.js
@@ -85,16 +85,19 @@ router.put("/:taskid", async (req,res) => {
 });
 
 // Delete  /api/tasks/:taskid --delete task  by task ID and to do that have to have correct authorization--otherwise denied ability to delete that task//
+// this says -- using the router.delete function to find a task by its ID---asyncronous set up---for the request from user to the response from database--this function try
+// find the object called taskId in the request parameters---give time or await to find the task by that taskid using the findById--once found populate that task in 
+// the associated project-----if the task is not found by the task id--return to the user the error message 404 with detailed message "Task not found by that taskID"
 router.delete("/:taskId", async (req,res) => {
   try {
     const { taskId } = req.params;
      // find the task and populate the project//
      const task = await Task.findById(taskId).populate("project");
      if(!task) {
-      return res.status(404).json({ message: "Task not found"});
+      return res.status(404).json({ message: "Task not found in the project"});
      }
+// Auth check--if task by that taskID is found --see if the user owns the parent project first before continuing//
 
-     
   } catch (
    
   ) {
